3301c815394ab4d11479fb2977eab106bf953b9c
Phuong Cao <pcao3@illinois.edu>
2016-02-18 13:59:08 -0600

    Update README and minor fixes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
-MAINTAINER Phuong Cao <pcao3 at illinois dot edu>
 FROM ubuntu
+MAINTAINER Phuong Cao <pcao3 at illinois dot edu>
 RUN apt-get update && apt-get install -y debootstrap
 ADD build_image.sh /usr/bin/
+VOLUME /tmp/timemachine
 ENTRYPOINT ["/usr/bin/build_image.sh"]
--- build_image.sh
+++ build_image.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
 # default variables
-tmp_dir=/tmp/timemachine/
+tmp_dir=/tmp/timemachine
 default_distribution=jessie
 default_datetime=20151231
-
 # validating number of inputs
 if [ "$#" -ne 2 ]; then
-    echo "Usage example: docker run -t --rm --privileged -v $tmp_dir:$tmp_dir csldepend/timemachine $default_distribution $default_datetime"
+    echo "Example usage: docker run -t --rm --privileged -v $tmp_dir/:$tmp_dir/ csldepend/timemachine $default_distribution $default_datetime"
     exit 1
 fi
-distribution=$1
-datetime=$2
+distribution=$2
+datetime=$3
 
 # create image using debootstrap
 mkdir -p tmp_dir && cd tmp_dir && \
 debootstrap $distribution ${distribution}-${datetime} http://snapshot.debian.org/archive/debian/${datetime}T000000Z/ && \
 tar cvf ${distribution}-${datetime}.tar ${distribution}-${datetime}/* && \
-echo "Image created! Import by: docker import $tmp_dir/${distribution}-${datetime}.tar ${distribution}:${datetime}"
+echo "Image created! Import by: \n docker import $tmp_dir/${distribution}-${datetime}.tar ${distribution}:${datetime}"
